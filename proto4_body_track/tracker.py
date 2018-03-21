e5ee2e88030ca96717406102f5122d680fe6c4a7
kartik9k <kartik9k@gmail.com>
2018-03-21 17:58:14 +0530

    Added from linux

--- proto4_body_track/tracker.py
+++ proto4_body_track/tracker.py
@@ -4,27 +4,28 @@ import argparse
 import imutils
 import cv2
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-b", "--buffer", type=int, default=64, help="max buffer size")
-args = vars(ap.parse_args())
-
 colors_to_track = [[(80, 100, 100), (100, 255, 255)], [(50, 100, 100), (70, 255, 255)]]
 det_points = []
 
 for i in range(len(colors_to_track)):
-	det_points.append(deque(maxlen=args["buffer"]))
+	det_points.append(deque(maxlen=5))
 
 camera = cv2.VideoCapture(0)
-
+# 
 while True:
 	(grabbed, frame) = camera.read()
-	frame = imutils.resize(frame, width=600)
+	frame = imutils.resize(frame, width=1366, height=768)
+	# frame = imutils.resize(frame, width=600)
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	
+	# canvas = np.full((768, 1366, 3), 255)
+	# canvas = cv2.imread("stickman.jpg")
 	
 	for k in range(len(colors_to_track)):
 		mask = cv2.inRange(hsv, colors_to_track[k][0], colors_to_track[k][1])
 		mask = cv2.erode(mask, None, iterations=2)
 		mask = cv2.dilate(mask, None, iterations=2)
+
 		cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
 			cv2.CHAIN_APPROX_SIMPLE)[-2]
 		center = None
@@ -38,14 +39,16 @@ while True:
 					(0, 255, 255), 2)
 				cv2.circle(frame, center, 5, (0, 0, 255), -1)
 		det_points[k].appendleft(center)
-		# mask = np.full((450, 600, 3), 255)
-		mask = frame
+		
+		# mask = np.full((768, 1366, 3), 255)
+		canvas = frame
+		
 		for i in xrange(1, len(det_points[k])):
 			if det_points[k][i - 1] is None or det_points[k][i] is None:
 				continue
-			thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-			cv2.line(mask, det_points[k][i - 1], det_points[k][i], (0, 0, 255), thickness)
-	cv2.imshow("Frame", mask)
+			thickness = int(np.sqrt(5 / float(i + 1)) * 2.5)
+			cv2.line(canvas, det_points[k][i - 1], det_points[k][i], (0, 0, 255), thickness)
+		cv2.imshow("Frame", canvas)
 
 	key = cv2.waitKey(1) & 0xFF
 	if key == ord("q"):
