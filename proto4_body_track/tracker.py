ab3b1a935a52437baf50a77fbec2c45719f8e5fb
kartik9k <kartik9k@gmail.com>
2018-03-22 18:27:46 +0530

    Added stickman

--- proto4_body_track/tracker.py
+++ proto4_body_track/tracker.py
@@ -8,18 +8,20 @@ colors_to_track = [[(80, 100, 100), (100, 255, 255)], [(50, 100, 100), (70, 255,
 det_points = []
 
 for i in range(len(colors_to_track)):
-	det_points.append(deque(maxlen=5))
+	det_points.append(deque(maxlen=2))
 
 camera = cv2.VideoCapture(0)
 # 
 while True:
 	(grabbed, frame) = camera.read()
+	frame = cv2.flip(frame, 1)
 	frame = imutils.resize(frame, width=1366, height=768)
 	# frame = imutils.resize(frame, width=600)
+	
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 	
 	# canvas = np.full((768, 1366, 3), 255)
-	# canvas = cv2.imread("stickman.jpg")
+	canvas = cv2.imread("stickman.jpg")
 	
 	for k in range(len(colors_to_track)):
 		mask = cv2.inRange(hsv, colors_to_track[k][0], colors_to_track[k][1])
@@ -41,13 +43,14 @@ while True:
 		det_points[k].appendleft(center)
 		
 		# mask = np.full((768, 1366, 3), 255)
-		canvas = frame
+		# canvas = frame
 		
 		for i in xrange(1, len(det_points[k])):
 			if det_points[k][i - 1] is None or det_points[k][i] is None:
 				continue
 			thickness = int(np.sqrt(5 / float(i + 1)) * 2.5)
 			cv2.line(canvas, det_points[k][i - 1], det_points[k][i], (0, 0, 255), thickness)
+			cv2.line(canvas, (683, 300), det_points[k][i], (0, 0, 255), thickness)
 		cv2.imshow("Frame", canvas)
 
 	key = cv2.waitKey(1) & 0xFF
