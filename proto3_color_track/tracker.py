0ec84b919e0b1c1f428fa3c4c45d7b933fb1b1db
kartik9k <kartik9k@gmail.com>
2018-03-27 13:59:02 +0530

    Added touch ability

--- proto3_color_track/tracker.py
+++ proto3_color_track/tracker.py
@@ -4,17 +4,19 @@ import argparse
 import imutils
 import cv2
 from pymouse import PyMouse
+import serial
+import time 
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-b", "--buffer", type=int, default=64, help="max buffer size")
-args = vars(ap.parse_args())
+ser = serial.Serial('/dev/ttyACM0', 9600)
+
 mouse = PyMouse()
 
 lower = (80, 100, 100)
 upper = (100, 255, 255)
-pts = deque(maxlen=args["buffer"])
+pts = deque(maxlen=5)
 
 camera = cv2.VideoCapture(0)
+prev = 0
 
 while True:
 	(grabbed, frame) = camera.read()
@@ -42,6 +44,8 @@ while True:
 				(0, 255, 255), 2)
 			cv2.circle(frame, center, 5, (0, 0, 255), -1)
 
+		
+	
 	pts.appendleft(center)
 	mask = np.full((768, 1366, 3), 255)
 	# mask = frame
@@ -49,13 +53,23 @@ while True:
 		if pts[i - 1] is None or pts[i] is None:
 			continue
 
-		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+		thickness = int(np.sqrt(5 / float(i + 1)) * 2.5)
 		cv2.line(mask, pts[i - 1], pts[i], (0, 0, 255), thickness)
 		# x_int = np.interp(pts[i][0], [1, 450], [1, 1366])
 		# y_int = np.interp(pts[i][1], [1, 600], [1, 768])
 
 		# mouse.move(x_int, y_int)
 		mouse.move(pts[i][0], pts[i][1])
+
+	read = ser.readline()
+	if (read != ''):
+		read = int(read)
+		if read == 1:
+			if prev != 1:
+				x, y = mouse.position()
+				mouse.click(x, y, 1)
+
+		prev = read
 
 	cv2.imshow("Frame", mask)
 	key = cv2.waitKey(1) & 0xFF
